9959163d08ded9ba97825bf73faf0ec5c7239e2b
LeoAndo <leo.777.info@gmail.com>
2021-12-23 00:56:44 +0900

    refactor viewBinding instance release logic

--- OnsenStampRallyApp/app/src/main/java/com/onsenstamprallyapp/ui/list/OnsenListFragment.kt
+++ OnsenStampRallyApp/app/src/main/java/com/onsenstamprallyapp/ui/list/OnsenListFragment.kt
@@ -6,25 +6,19 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import com.onsenstamprallyapp.R
 import com.onsenstamprallyapp.databinding.FragmentOnsenListBinding
+import com.onsenstamprallyapp.ui.util.viewBindings
 
 /**
  * A simple [Fragment] subclass as the second destination in the navigation.
  */
 class OnsenListFragment : Fragment(R.layout.fragment_onsen_list) {
 
-    private var _binding: FragmentOnsenListBinding? = null
-    private val binding get() = _binding!!
+    private val binding by viewBindings(FragmentOnsenListBinding::bind)
     private val viewModel by viewModels<OnsenListViewModel>()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        _binding = FragmentOnsenListBinding.bind(view)
         // findNavController().navigate(R.id.action_SecondFragment_to_FirstFragment)
         viewModel.print()
-    }
-
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
     }
 }
