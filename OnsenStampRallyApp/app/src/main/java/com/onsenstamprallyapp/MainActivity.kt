eba4d95f1b39ad22247adf32fbc261efe5509742
LeoAndo <leo.777.info@gmail.com>
2021-12-22 23:47:52 +0900

    add Drawaer navigation

--- OnsenStampRallyApp/app/src/main/java/com/onsenstamprallyapp/MainActivity.kt
+++ OnsenStampRallyApp/app/src/main/java/com/onsenstamprallyapp/MainActivity.kt
@@ -1,61 +1,76 @@
 package com.onsenstamprallyapp
 
 import android.os.Bundle
-import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.WindowCompat
+import androidx.core.view.isVisible
+import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
-import android.view.Menu
-import android.view.MenuItem
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.snackbar.Snackbar
 import com.onsenstamprallyapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var navController: NavController
     private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        WindowCompat.setDecorFitsSystemWindows(window, false)
         super.onCreate(savedInstanceState)
-
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
-        setSupportActionBar(binding.toolbar)
-
-        val navController = findNavController(R.id.nav_host_fragment_content_main)
-        appBarConfiguration = AppBarConfiguration(navController.graph)
-        setupActionBarWithNavController(navController, appBarConfiguration)
-
-        binding.fab.setOnClickListener { view ->
+        setSupportActionBar(binding.appBarMain.toolbar)
+        binding.appBarMain.fab.setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAnchorView(R.id.fab)
                 .setAction("Action", null).show()
         }
+        navController = findNavController(R.id.nav_host_fragment_content_main)
+        observeDestination()
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.FirstFragment, R.id.SecondFragment
+            ), binding.drawerLayout
+        )
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        binding.navView.setupWithNavController(navController)
     }
 
+    // TODO option menuは蓋とじ-START
+    /*
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         // Inflate the menu; this adds items to the action bar if it is present.
         menuInflater.inflate(R.menu.menu_main, menu)
         return true
     }
+     */
+    // TODO option menuは蓋とじ-END
 
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        return when (item.itemId) {
-            R.id.action_settings -> true
-            else -> super.onOptionsItemSelected(item)
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
+    }
+
+    private fun observeDestination() {
+        navController.addOnDestinationChangedListener { n, destination, _ ->
+            updateContent(destination.id)
         }
     }
 
-    override fun onSupportNavigateUp(): Boolean {
-        val navController = findNavController(R.id.nav_host_fragment_content_main)
-        return navController.navigateUp(appBarConfiguration)
-                || super.onSupportNavigateUp()
+    private fun updateContent(destinationId: Int) {
+        when (destinationId) {
+            R.id.FirstFragment, R.id.SecondFragment -> {
+                supportActionBar?.show()
+                binding.navView.isVisible = true
+            }
+            else -> {
+                supportActionBar?.show()
+                binding.navView.isVisible = false
+                // binding.appBarMain.fab.hide()
+            }
+        }
     }
 }
